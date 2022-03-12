dec8b4a4acd9fc61708c57ab2f7b587619ec7a02
Pl. Petroff <plpetroff@gmail.com>
2022-03-13 01:58:36 +0200

    Creating BaseController

---
+++ CarBattery-2022/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBattery_2022.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class BaseController : Controller
+    {
+        
+    }
+}
--- CarBattery-2022/Controllers/HomeController.cs
+++ CarBattery-2022/Controllers/HomeController.cs
@@ -5,7 +5,7 @@
     using Microsoft.AspNetCore.Mvc;
     using System.Diagnostics;
 
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
