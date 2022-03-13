023573e47da34f2788e87794b4caa6f5da34d335
Pl. Petroff <plpetroff@gmail.com>
2022-03-13 02:41:42 +0200

    Added DbSets

--- CarBattery-2022.Infrastructure/Data/ApplicationDbContext.cs
+++ CarBattery-2022.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿namespace CarBattery_2022.Infrastructure.Data
 {
+    using CarBattery_2022.Infrastructure.Data.Models;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,39 @@
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+
+        public DbSet<Battery> Batteries { get; init; }
+
+        public DbSet<Brand> Brands { get; init; }
+
+        public DbSet<Capacity> Capacities { get; init; }
+
+        public DbSet<Amperage> Amperages { get; init; }
+
+        public DbSet<Technology> Technologies { get; init; }
+
+        public DbSet<Category> Categories { get; init; }
+
+        public DbSet<Terminals> Terminals { get; init; }
+
+        public DbSet<BoxType> BoxTypes { get; init; }
+
+        public DbSet<BatteryCode> BatteryCodes { get; init; }
+
+        public DbSet<Image> Images { get; init; }
+
+        public DbSet<Price> Prices { get; init; }
+
+        public DbSet<Length> Lengths { get; init; }
+
+        public DbSet<Width> Widths { get; init; }
+
+        public DbSet<Height> Heights { get; init; }
+
+        public DbSet<HeightPlus> HeightsPluses { get; init; }
+
+        public DbSet<Weight> Weights { get; init; }
     }
 }
