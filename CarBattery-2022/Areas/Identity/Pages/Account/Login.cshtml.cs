065237b656bfc96d037c53e1f98408d5acb86b97
Pl. Petroff <plpetroff@gmail.com>
2022-03-23 16:12:37 +0200

    .AddRoles<IdentityRole>()

--- CarBattery-2022/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CarBattery-2022/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -64,7 +64,7 @@ namespace CarBattery_2022.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            [Required(ErrorMessage = "Полето {0} е задължително")]
             [EmailAddress]
             public string Email { get; set; }
 
@@ -72,7 +72,7 @@ namespace CarBattery_2022.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            [Required(ErrorMessage = "Полето {0} е задължително")]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
--- CarBattery-2022/Program.cs
+++ CarBattery-2022/Program.cs
@@ -12,10 +12,13 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews()
     .AddMvcOptions(options =>
     {
